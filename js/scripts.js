62a71fc6b55eba5de920aa8479a5d853ef2d52e6
Carlos Trujillo <59846724+cetagostini@users.noreply.github.com>
2023-08-12 12:45:33 +0000

    Updating page

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.addEventListener('DOMContentLoaded', function() {
+    var images = document.querySelectorAll('.img-fluid');
+
+    images.forEach(function(img) {
+        img.onload = function() {
+            // Check if the image is more vertical (height > width)
+            if (img.naturalHeight > img.naturalWidth) {
+                img.classList.add('vertical-aspect');
+            }
+        }
+    });
+});
